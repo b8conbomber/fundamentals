0eb2891b263fe3c66458d62a080440da2613fda4
Jason Diehl <jasondiehl04@gmail>
2015-11-16 18:47:12 -0500

    save

--- py/hello.py
+++ py/hello.py
@@ -5,44 +5,35 @@
 import skilstak.colors as c
 import time
 
-def plain(message):
+def print_plain(message):
     print(c.cl + c.x +message)
 
-def color(message):
+def print_color(message):
     print(c.cl,end="")
     while True:
         print(c.rc() + message + c.x, end=" ")
 
-def multi(message):
+def print_multi(message):
     """Mulit Colord"""
     while True:
         print(c.cl + c.multi(message))
         time.sleep(0.5)
 
-if __name__ == "__main__":
+def parse_args():
     import sys
     
-    who = "World"
-    option = ""
-    message = ""
+    p = {
+        "who": "World",
+        "option": ""
+    }
 
     if len(sys.argv) > 2:
-        option = sys.argv[1]
-        who = sys.argv[2]
+        p["option"] = sys.argv[1]
+        p["who"] = sys.argv[2]
     elif len(sys.argv) == 2:
         if sys.argv[1].startswith("-"):
-            option = sys.argv[1]
+           p["option"] = sys.argv[1]
         else:
-            who = sys.argv[1]
+            p["who"] = sys.argv[1]
 
-    message = "Hello, " + who + "!"
-
-    try:
-        if option == "-c":
-            color(message)
-        elif option == "-m":
-            multi(message)
-        else:
-            plain(message)
-    except KeyboardInterrupt:
-        exit(0)
+    return p
